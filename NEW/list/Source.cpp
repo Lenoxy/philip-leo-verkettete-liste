b8e5a1b68212590bbd699366f895c1aac7b2d371
Leo Scherer <lenoxy@gmx.ch>
2019-12-21 11:10:21 +0100

    Struct

--- NEW/list/Source.cpp
+++ NEW/list/Source.cpp
@@ -2,5 +2,12 @@
 #include <stdio.h>
 
 void main() {
+	typedef struct Grafikkarte {
+		char name[40];
+		char hersteller[40];
+		int herstellungsjahr;
+		struct Grafikkarte* pNext;
+		struct Grafikkarte* pPrev;
+	} struGrafikkarte;
 
 }
