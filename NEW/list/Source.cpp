78bf98ba9342558338eba2840a79cddbccc5c7fd
BBZW-Emmen;BBZW-Sursee; Baumann Philip <philip_baumann@sluz.ch>
2019-12-21 11:28:09 +0100

    PHILIP - Methoden Deklarierung

--- NEW/list/Source.cpp
+++ NEW/list/Source.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 void main() {
+
+	// Typedefinition
 	typedef struct Grafikkarte {
 		char name[40];
 		char hersteller[40];
@@ -9,5 +11,24 @@ void main() {
 		struct Grafikkarte* pNext;
 		struct Grafikkarte* pPrev;
 	} struGrafikkarte;
+
+	// Function declaration
+	struGrafikkarte* CreateList(int Amount);
+	void DeleteList(struGrafikkarte **pList);
+	void DeleteElement(struGrafikkarte **pList, char name, char hersteller);
+	void SortList();
+	void PrintList(struGrafikkarte **pList, int Amount);
+	void QuitApplication();
+	void PrintUserInterface();
+
+	/*
+	*Creates a list with a defined amount of elements
+	*@Return
+	*/
+
+
+
+
+
 
 }
